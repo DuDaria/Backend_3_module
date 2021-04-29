e2282bd62660b4292b1766d57def560432a9b382
DuDaria <60156592+DuDaria@users.noreply.github.com>
2021-04-29 23:36:21 +0400

    Update Homework_2

--- blogproject/advito/admin.py
+++ blogproject/advito/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Profile, Post, CategoryPost, HistoryPost, FavoritePost, Comment
+from .models import Profile, Post, CategoryPost, FavoritePost, Comment, Review, Message
 
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(CategoryPost)
-admin.site.register(HistoryPost)
 admin.site.register(FavoritePost)
 admin.site.register(Comment)
+admin.site.register(Review)
+admin.site.register(Message)
--- blogproject/advito/models.py
+++ blogproject/advito/models.py
@@ -22,7 +22,7 @@ class Profile(models.Model):
     messages = models.ManyToManyField(User, blank=True, verbose_name="Сообщения пользователю", related_name='messages') 
 
     def __str__(self):
-        return f'Profile for user - {self.user}'
+        return f'Profile: {self.user}'
 
 
 class CategoryPost(models.Model):
@@ -30,31 +30,22 @@ class CategoryPost(models.Model):
     name_category = models.CharField(verbose_name="Категория", max_length=100)
 
     def __str__(self):
-        return f'Category - {self.name_category}'
+        return f'Category: {self.name_category}'
 
 
 class Post(models.Model):
-    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(CategoryPost, on_delete=models.CASCADE, verbose_name="Категория")
-    price = models.IntegerField(verbose_name="Цена")
+    price = models.FloatField(verbose_name="Цена", blank=True)
     name_descript = models.CharField(max_length=200,  blank=True, verbose_name="Название объявления")
     description = models.TextField(max_length=1000, blank=True, verbose_name="Описание объявления")
     image = models.ImageField(upload_to=user_directory_path, verbose_name="Фото объявления")
     date_pub = models.DateTimeField(default=timezone.now, verbose_name="Дата объявления")
     date_adit = models.DateTimeField(default=timezone.now, verbose_name="Дата редактирования")
+    user = models.ManyToManyField(User, blank=True, related_name='user') 
     
     def __str__(self):
-        return f'Post - {self.name_descript}; {self.category}; Autor - {self.author}'
-
-
-class HistoryPost(models.Model):
-    """ История объявления"""
-    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    post = models.ManyToManyField(Post, blank=True, related_name='post')
-    date_create = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
-
-    def __str__(self):
-        return f'Post - {self.date_create}; date_create - {self.date_create}'
+        return f'Post: {self.name_descript}; {self.category}; Autor: {self.author}'
 
 
 class FavoritePost(models.Model):
@@ -64,11 +55,36 @@ class FavoritePost(models.Model):
     
 
 class Comment(models.Model):
-    """Отзыв об объявлении"""
+    """Комментарий к посту"""
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=700)
     in_post = models.ForeignKey(Post, on_delete=models.CASCADE)
     date_publish = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f'Autor - {self.author}; Date_publish - {self.date_publish}'
+        return f'Autor: {self.author}; Date_publish: {self.date_publish}'
+
+
+class Review(models.Model):
+    """Отзыв о пользователе"""
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, verbose_name="Автор отзыва")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Кому отзыв")
+    rating = models.IntegerField(verbose_name="Оценка", null=True, blank=True)
+    text = models.TextField(verbose_name="Отзыв", blank=True, max_length=300)
+    date_pub = models.DateTimeField(default=timezone.now, verbose_name="Дата создания")
+    date_edit = models.DateTimeField(default=timezone.now, verbose_name="Дата редактирования")
+
+    def __str__(self):
+        return f'Autor: {self.author}; User: {self.user}; Date_publish: {self.date_pub}'
+    
+
+class Message(models.Model):
+    """Сообщение"""
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, verbose_name="Автор сообщения")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Кому сообщение")
+    text = models.TextField(verbose_name="Сообщение", blank=True, max_length=300)
+    date_pub = models.DateTimeField(default=timezone.now, verbose_name="Дата создания")
+    date_edit = models.DateTimeField(default=timezone.now, verbose_name="Дата редактирования")
+
+    def __str__(self):
+        return f'Autor: {self.author}; User: {self.user}; Date_publish: {self.date_pub}'
